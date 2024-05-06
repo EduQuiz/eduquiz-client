4b888041f64fd901a2264ba72f6641da890deefb
Lucas Marcos <lucasmarcos@live.com>
2024-05-06 19:28:22 -0300

    config files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,20 @@
-import type {Config} from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-    content: [
-        './pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './components/**/*.{js,ts,jsx,tsx,mdx}',
-        './app/**/*.{js,ts,jsx,tsx,mdx}',
-        './layouts/**/*.{js,ts,jsx,tsx,mdx}'
-    ],
-    theme: {
-        extend: {
-            colors: {
-                primary: '#00253A',
-                secondary: '#1d232a',
-            }
-        },
-
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./layouts/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00253A",
+        secondary: "#1d232a",
+      },
     },
-    plugins: [require("daisyui")],
-
-}
-export default config
+  },
+  plugins: [require("daisyui")],
+};
+export default config;
