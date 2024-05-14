d99aa42e8aec9336b539de4bcded70f64a594a33
Lucas Marcos <lucasmarcos@live.com>
2024-05-14 17:09:31 -0300

    login redirect

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,25 +8,17 @@ import { useEffect } from "react";
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     const { user_id } = parseCookies();
+  }, []);
 
-    if (
-      !user_id &&
-      Component.name !== "Login" &&
-      Component.name !== "Register"
-    ) {
-      document.location = "/login";
-    }
-  });
-
+  /*
   if (Component.name === "Login" || Component.name === "Register") {
     return <Component {...pageProps} />;
   }
+  */
 
   return (
-    <>
-      <Sidebar>
-        <Component {...pageProps} />
-      </Sidebar>
-    </>
+    <Sidebar>
+      <Component {...pageProps} />
+    </Sidebar>
   );
 }
