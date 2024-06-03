0671b6e999fe49e7eb10f1705fe3e8b3211a2f89
Lucas Marcos <lucasmarcos@live.com>
2024-06-03 14:49:25 -0300

    comps

--- src/components/card_quiz.tsx
+++ src/components/card_quiz.tsx
@@ -1,63 +1,36 @@
-import ClipboardJS from "clipboard";
 import Link from "next/link";
 
-interface Props {
-  id: string;
-  title: string;
-  description: string;
-}
-
-export default function CardQuiz(props: Props) {
-  const { id } = props;
-
-  async function copie() {
-    const clipboard = new ClipboardJS(".btn-copiar", {
-      text: () => `http://localhost:3000/quiz/${props.id}/select_answer`,
-    });
-
-    clipboard.on("success", (e) => {
-      console.log("Texto copiado com sucesso!");
-      e.clearSelection();
-    });
-
-    clipboard.on("error", (e) => {
-      console.error(
-        "Erro ao copiar texto para a área de transferência",
-        e.action,
-      );
-    });
-  }
-
-  return (
+const CardQuiz: React.FC<{ id: string; title: string; description: string }> =
+  ({ id, title, description }) => (
     <div className="card card-compact w-96 bg-base-100 shadow-xl min-w-[28rem]">
       <div className="card-body">
         <div className="flex justify-between">
-          <h2 className="card-title">{props.title}</h2>
-          <div className="badge badge-secondary h-6">{props.id}</div>
+          <h2 className="card-title">{title}</h2>
         </div>
-        <p>{props.description}</p>
+
+        <p>{description}</p>
+
         <div className="card-actions justify-end">
+          <Link href={`/questionario/${id}`}>
+            <button className="btn" type="button">
+              Editar
+            </button>
+          </Link>
+
           <Link href={`/responder/${id}`}>
             <button className="btn" type="button">
               Responder
             </button>
           </Link>
 
-          <Link href="/quiz/[id]/score" as={`/quiz/${props.id}/score`}>
-            <button className="btn btn-primary" type="button">
-              Visualizar
-            </button>
-          </Link>
-
-          <Link
-            href={`https://wa.me/?text=http://localhost:3000/quiz/${props.id}/select_answer`}
-          >
-            <button onClick={copie} className="btn btn-primary" type="button">
-              Compartilhar
+          <Link href={`/resultados/${id}`}>
+            <button className="btn" type="button">
+              Resultados
             </button>
           </Link>
         </div>
       </div>
     </div>
   );
-}
+
+export default CardQuiz;
